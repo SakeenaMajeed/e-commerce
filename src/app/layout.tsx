9c39fe96f1922689c8df43b106dd6c731e9a60d4
Sakeena Majeed <sakeenamajeed398@gmail.com>
2024-10-02 15:46:09 +0500

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import ClientLayout from "./clientlayout";
+import ClientLayout from "./ClientLayout";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
